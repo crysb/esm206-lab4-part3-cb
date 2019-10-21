f75cb597d2056fb2332f9a86787e5fad1700bd0a
crysb <crysbae@gmail.com>
2019-10-21 13:47:24 -0700

    Cleanup disease burden dataframe

--- lab4_part3.R
+++ lab4_part3.R
@@ -1,7 +1,21 @@
 
+#####################
+#  R Lab 4, Part 3  #
+#  October 21, 2019 #
+#####################
+
 # Read in packages
 library(tidyverse)
+library(janitor)
 library(here)
 
-# Read in disease burden dataset
-read_csv("data/disease_burden.csv")
+# Read in disease burden dataset using {here} and {janitor}
+db <- read_csv(here::here("data","disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+
+
+
+# Output graphs
+#ggsave()
